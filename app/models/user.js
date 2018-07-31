79aa320170ec3b6b55320ba187c5b9b042e87a9d
nmonty9 <namontalbano@email.neit.edu>
2018-07-31 15:50:30 -0400

    fixed userSchema

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
-var acountsSchema = mongoose.Schema({
+var userSchema = mongoose.Schema({
 
     local            : {
         email        : String,
@@ -45,4 +45,4 @@ userSchema.methods.validPassword = function(password) {
 };
 
 // create the model for users and expose it to our app
-module.exports = mongoose.model('Accounts', userSchema);
+module.exports = mongoose.model('User', userSchema);
